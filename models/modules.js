adbf3f0883532e0f3f19354c234edd47b956d4a5
p291107 <stephanie.greatbatch@gmail.com>
2025-07-30 21:14:52 +0800

    practical 3

--- models/modules.js
+++ models/modules.js
@@ -1,75 +1,92 @@
-const { query } = require('../database');
-const { EMPTY_RESULT_ERROR, SQL_ERROR_CODE, UNIQUE_VIOLATION_ERROR } = require('../errors');
+const { PrismaClient, Prisma } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 module.exports.create = function create(code, name, credit) {
-    const sql = 'CALL create_module($1, $2, $3)';
-    return query(sql, [code, name, credit])
-        .then(function (result) {
-            console.log('Module created successfully');
-        })
-        .catch(function (error) {
-            throw error;
-        });
-};
+    return prisma.module.create({
+        //TODO: Add data
+        data: {
+            modCode: code,
+            modName: name,
+            creditUnit: parseInt(credit)
+        }
+    }).then(function (module) {
+        //TODO: Return module
+        return module;
+    }).catch(function (error) {
+            // Prisma error codes: https://www.prisma.io/docs/orm/reference/error-reference#p2002
+            // TODO: Handle Prisma Error, throw a new error if module already exists                    
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2002') {
+                throw new Error(`The Module ${code} already exists`);
+            }
+        }
+        throw error;
+    });
+}
 
 module.exports.retrieveByCode = function retrieveByCode(code) {
-    const sql = `SELECT * FROM module WHERE mod_code = $1`;
-    return query(sql, [code]).then(function (result) {
-        const rows = result.rows;
-
-        if (rows.length === 0) {
-            // Note: result.rowCount returns the number of rows processed instead of returned
-            // Read more: https://node-postgres.com/apis/result#resultrowcount-int--null
-            throw new EMPTY_RESULT_ERROR(`Module ${code} not found!`);
+    return prisma.module.findUnique({
+        //TODO: Add where
+        where: {
+            modCode: code
         }
-
-        return rows[0];
+    }).then(function (module) {
+        // Prisma error codes: https://www.prisma.io/docs/orm/reference/error-reference#p2025
+        // TODO: Handle Prisma Error, throw a new error if module is not found                            
+        // TODO: Return module
+        if (!module) {
+            throw new Error(`The Module ${code} is not found!`);
+        }
+        return module;
     });
 };
-
+ 
 module.exports.deleteByCode = function deleteByCode(code) {
-    // Note:
-    // If using raw sql: Can use result.rowCount to check the number of rows affected
-    // But if using function/stored procedure, result.rowCount will always return null
-    const sql = 'CALL delete_module($1)';
-    return query(sql, [code])
-        .then(function (result) {
-            console.log('Module deleted successfully');
-        }).catch(function (error) {
-            throw error;
-        });
+    return prisma.module.delete({
+        //TODO: Add where
+        where: {
+            modCode: code
+        }
+    }).then(function (module) {
+        // Leave blank
+    }).catch(function (error) {
+        // Prisma error codes: https://www.prisma.io/docs/orm/reference/error-reference#p2025
+        // TODO: Handle Prisma Error, throw a new error if module is not found                            
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2025') {
+                throw new Error(`The Module ${code} is not found!`);
+            }
+        }
+        throw error;
+    })
 };
-
+ 
 module.exports.updateByCode = function updateByCode(code, credit) {
-    // Note:
-    // If using raw sql: Can use result.rowCount to check the number of rows affected
-    // But if using function/stored procedure, result.rowCount will always return null
-    const sql = 'CALL update_module($1, $2)';
-    return query(sql, [code, credit])
-        .then(function (result) {
-            console.log('Module updated successfully');
-        }).catch(function (error) {
-            throw error;
-        });
-};
-
-module.exports.retrieveAll = function retrieveAll() {
-    const sql = `SELECT * FROM module`;
-    return query(sql).then(function (result) {
-        return result.rows;
+    return prisma.module.update({
+        //TODO: Add where and data
+        where: {
+            modCode: code
+        },
+        data: {
+            creditUnit: parseInt(credit)
+        }
+    }).then(function (module) {
+        // Leave blank
+    }).catch(function (error) {
+        // Prisma error codes: https://www.prisma.io/docs/orm/reference/error-reference#p2025
+        // TODO: Handle Prisma Error, throw a new error if module is not found                            
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2025') {
+                throw new Error(`The Module ${code} is not found!`);
+            }
+        }
+        throw error;    
     });
 };
-
-module.exports.retrieveBulk = function retrieveBulk(codes) {
-    const sql = 'SELECT * FROM module WHERE code IN ($1)';
-    return query(sql, [codes]).then(function (response) {
-        const rows = response.rows;
-        const result = {};
-        for (let i = 0; i < rows.length; i += 1) {
-            const row = rows[i];
-            const code = row.code;
-            result[code] = row;
-        }
-        return result;
+ 
+module.exports.retrieveAll = function retrieveAll() {
+    // TODO: Return all modules
+    return prisma.module.findMany().then(function (modules) {
+        return modules;
     });
 };
---
+++ prisma/migrations/20250730125620_finalize_all_initial_models/migration.sql
@@ -0,0 +1,58 @@
+/*
+  Warnings:
+
+  - You are about to drop the `StaffDependent` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "stud_mod_performance" ALTER COLUMN "mark" DROP NOT NULL,
+ALTER COLUMN "grade" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "student" ALTER COLUMN "mobile_phone" DROP NOT NULL,
+ALTER COLUMN "home_phone" DROP NOT NULL;
+
+-- DropTable
+DROP TABLE "StaffDependent";
+
+-- CreateTable
+CREATE TABLE "staff_dependent" (
+    "staff_no" CHAR(4) NOT NULL,
+    "dependent_name" VARCHAR(30) NOT NULL,
+    "relationship" VARCHAR(20) NOT NULL,
+
+    CONSTRAINT "staff_dependent_pkey" PRIMARY KEY ("staff_no","dependent_name")
+);
+
+-- AddForeignKey
+ALTER TABLE "course" ADD CONSTRAINT "course_offered_by_fk" FOREIGN KEY ("offered_by") REFERENCES "department"("dept_code") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "department" ADD CONSTRAINT "dept_hod_fk" FOREIGN KEY ("hod") REFERENCES "staff"("staff_no") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "module" ADD CONSTRAINT "mod_mod_coord_fk" FOREIGN KEY ("mod_coord") REFERENCES "staff"("staff_no") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "pre_requisite" ADD CONSTRAINT "pre_requisite_mod_code_fk" FOREIGN KEY ("mod_code") REFERENCES "module"("mod_code") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "pre_requisite" ADD CONSTRAINT "pre_requisite_requisite_fk" FOREIGN KEY ("requisite") REFERENCES "module"("mod_code") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "staff" ADD CONSTRAINT "staff_dept_code_fk" FOREIGN KEY ("dept_code") REFERENCES "department"("dept_code") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "staff_dependent" ADD CONSTRAINT "staff_dependent_staff_no_fk" FOREIGN KEY ("staff_no") REFERENCES "staff"("staff_no") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "stud_mod_performance" ADD CONSTRAINT "stud_mod_performance_adm_no_fkey" FOREIGN KEY ("adm_no") REFERENCES "student"("adm_no") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "stud_mod_performance" ADD CONSTRAINT "stud_mod_performance_mod_registered_fkey" FOREIGN KEY ("mod_registered") REFERENCES "module"("mod_code") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "student" ADD CONSTRAINT "student_crse_code_fk" FOREIGN KEY ("crse_code") REFERENCES "course"("crse_code") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "student" ADD CONSTRAINT "student_nationality_fk" FOREIGN KEY ("nationality") REFERENCES "country"("country_name") ON DELETE NO ACTION ON UPDATE NO ACTION;
